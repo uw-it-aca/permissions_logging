31d37466e50af66363f13c2e05641a2752400add
Patrick Michaud <pmichaud@uw.edu>
2014-01-16 12:05:08 -0800

    An initial check-in of code, and distribution tools

---
+++ permissions_logging.py
@@ -0,0 +1,63 @@
+"""
+A collection of log handlers that allow file permissions to be specified.
+"""
+import logging
+import logging.handlers
+import os
+
+
+class FileHandler(logging.FileHandler):
+    """
+    A subclass of logging.FileHandler that accepts a permissions argument.
+
+    The file used for logging will have the value of the permissions keyword
+    value, e.g. for unix:  permissions=0o664
+    """
+    def __init__(self, filename, permissions=0o644):
+        self.permissions = permissions
+        logging.FileHandler.__init__(self, filename, **kwargs)
+
+    def _open(self):
+        stream = logging.FileHandler._open(self)
+        os.chmod(self.baseFilename, self.permissions)
+
+        return stream
+
+
+class TimedRotatingFileHandler(FileHandler, handlers.TimedRotatingFileHandler):
+    """
+    A subclass of logging.handlers.TimedRotatingFileHandler that accepts
+    a permissions argument.
+
+    The file used for logging will have the value of the permissions keyword
+    value, e.g. for unix:  permissions=0o664
+    """
+    def __init__(self, filename, permissions=0o644, **kwargs):
+        self.permissions = permissions
+        handlers.TimedRotatingFileHandler.__init__(self, filename, **kwargs)
+
+
+class WatchedFileHandler(FileHandler, handlers.WatchedFileHandler):
+    """
+    A subclass of logging.handlers.WatchedFileHandler that accepts
+    a permissions argument.
+
+    The file used for logging will have the value of the permissions keyword
+    value, e.g. for unix:  permissions=0o664
+    """
+    def __init__(self, filename, permissions=0o644, **kwargs):
+        self.permissions = permissions
+        handlers.WatchedFileHandler.__init__(self, filename, **kwargs)
+
+
+class RotatingFileHandler(FileHandler, handlers.RotatingFileHandler):
+    """
+    A subclass of logging.handlers.RotatingFileHandler that accepts
+    a permissions argument.
+
+    The file used for logging will have the value of the permissions keyword
+    value, e.g. for unix:  permissions=0o664
+    """
+    def __init__(self, filename, permissions=0o644, **kwargs):
+        self.permissions = permissions
+        handlers.RotatingFileHandler.__init__(self, filename, **kwargs)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="PermissionsLogging",
+    version="1.0",
+    author = "Patrick Michaud",
+    author_email = "pmichaud@uw.edu",
+    description = "Python logging handlers that accept file permissions",
+    license = "Apache 2.0",
+    keywords = "logging permissions",
+)
